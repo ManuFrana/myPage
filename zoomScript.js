c5e96a2eed69099af3cf048512cb845380e8b072
Manuel Frana <manuel.frana@avature.net>
2024-07-29 22:17:24 +0200

    Initial Commit

---
+++ zoomScript.js
@@ -0,0 +1,65 @@
+console.clear();
+
+gsap.registerPlugin(ScrollTrigger);
+
+
+let showContent = false;
+let mainImage = document.querySelector(".zoom-image-container");
+
+
+window.addEventListener("load", () => {
+    gsap
+        .timeline({
+            scrollTrigger: {
+                trigger: ".zoom-wrapper",
+                start: "top top",
+                end: "+=150%",
+                pin: true,
+                scrub: true,
+                markers: false,
+                onUpdate: (self) => {
+                    const progress = self.progress; 
+                    if (progress >= 0.90) {
+                        triggerShowPageContentLogic();
+                    } else {
+                        if (showContent) {
+                            mainImage.style.visibility = "visible";
+                            dispatchHideContentEvent();
+                        }
+                    }
+                }
+            }
+        })
+        .to(".zoom-in-image", {
+            scale: 2,
+            z: 370,
+            transformOrigin: "center center",
+            ease: "power1.inOut",
+        })
+        .to(
+            ".section.first-content",
+            {
+                scale: 1.1,
+                transformOrigin: "center center",
+                ease: "power1.inOut",
+
+            },
+            "<"
+        );
+});
+
+function triggerShowPageContentLogic() {
+    showContent = true;
+    mainImage.style.visibility = "hidden";
+    dispatchShowContentEvent();
+}
+
+function dispatchShowContentEvent() {
+    const event = new CustomEvent("startAnimationShow", {});
+    window.dispatchEvent(event);
+}
+
+function dispatchHideContentEvent() {
+    const event = new CustomEvent("mainpage-content-hide", {});
+    window.dispatchEvent(event);
+}
